af16717245616ac12c124470e900db9f28765241
Luis Felipe Borges Silva <58269629+Lipekline@users.noreply.github.com>
2021-02-12 16:00:37 -0300

    background design

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$header = $('header'),
+$navLink = $header.find('nav a');
+
+$(function(){
+
+    $(window).scroll(function(){
+        var wScroll = $(this).scrollTop();
+
+        if(wSscroll > $('.apresentacao').height()){
+            
+        }
+    })
+})
